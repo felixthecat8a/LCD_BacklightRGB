4a04dc6b65a3a0038cd802243c969396ed7c232b
Felix <fo59paz@yahoo.com>
2024-05-28 19:30:51 -0500

    added clamp method

--- src/LCD_BacklightRGB.cpp
+++ src/LCD_BacklightRGB.cpp
@@ -5,24 +5,35 @@
 #define COMMON_ANODE
 
 LCD_BacklightRGB::LCD_BacklightRGB(int redPin, int greenPin, int bluePin) {
-    _redPin = redPin;
-    _greenPin = greenPin;
-    _bluePin = bluePin;
+  _redPin = redPin;
+  _greenPin = greenPin;
+  _bluePin = bluePin;
 }
 
 void LCD_BacklightRGB::begin() {
-    pinMode(_redPin, OUTPUT);
-    pinMode(_greenPin, OUTPUT);
-    pinMode(_bluePin, OUTPUT);
+  pinMode(_redPin, OUTPUT);
+  pinMode(_greenPin, OUTPUT);
+  pinMode(_bluePin, OUTPUT);
+}
+
+int LCD_BacklightRGB::clamp(int value) {
+  if (value < minRGB) return minRGB;
+  if (value > maxRGB) return maxRGB;
+  return value;
 }
 
 void LCD_BacklightRGB::setRGB(int red, int green, int blue) {
+  red = clamp(red);
+  green = clamp(green);
+  blue = clamp(blue);
+
   #ifdef COMMON_ANODE
     red = 255 - red;
     green = 255 - green;
     blue = 255 - blue;
   #endif
-    analogWrite(_redPin, red);
-    analogWrite(_greenPin, green);
-    analogWrite(_bluePin, blue);
+
+  analogWrite(_redPin, red);
+  analogWrite(_greenPin, green);
+  analogWrite(_bluePin, blue);
 }
--- src/LCD_BacklightRGB.h
+++ src/LCD_BacklightRGB.h
@@ -1,14 +1,16 @@
-// LCD_BacklightRGB.h
+// LCD_BacklightRGB.h v1.0.2
 #ifndef LCD_BACKLIGHTRGB_H
 #define LCD_BACKLIGHTRGB_H
 
 class LCD_BacklightRGB {
 private:
-    int _redPin, _greenPin, _bluePin;
+  int _redPin, _greenPin, _bluePin;
+  const int minRGB = 0, maxRGB = 255;
+  int clamp(int value);
 public:
-    LCD_BacklightRGB(int redPin, int greenPin, int bluePin);
-    void begin();
-    void setRGB(int red, int green, int blue);
+  LCD_BacklightRGB(int redPin, int greenPin, int bluePin);
+  void begin();
+  void setRGB(int red, int green, int blue);
 };
 
 #endif
