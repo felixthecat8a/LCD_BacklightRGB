9a86153c1ee833ce5e3fd4ce30eff3a30afb9a0f
Felix <fo59paz@yahoo.com>
2024-10-19 11:43:28 -0500

    updated examples

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -2,29 +2,29 @@
  * HelloWorld.ino
  * For an RGB LCD with 18 pins, set pin 15 to HIGH.
  * Pin 16 is red, pin 17 is green and pin 18 is blue.
+ * On the Arduino board, choose 3 PWM pins (denoted by ~)
 */
 
 #include <LiquidCrystal.h>
-
 #include "LCD_BacklightRGB.h"
-
+// Set the pins for the LCD display.
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-// Choose three PWM pins (denoted by ~)
+// Set the RGB pins for the RGB LCD. Use PWM pins denoted by a ~ symbol.
 const int redPin = 6, greenPin = 9, bluePin = 10;
 LCD_BacklightRGB backlight(redPin, greenPin, bluePin);
 
 void setup() {
-  // Initiate the LCD
+  // Initiate the LCD.
   lcd.begin(16, 2);
-  // initiate the backlight RGB pins
+  // Initiate the RGB pins.
   backlight.begin();
-  // Set the brightness level (0 - 255).
-  // If the color is not set, default value is 255.
-  backlight.setBrightness(130);
-  // Set the backlight using RGB values
-  // If the color is not set, the default color is (0, 128, 128)
-  backlight.setRGB(0, 255, 255);
+  // Optional: Set the brightness level (0 - 255). (Defaults to 255 if not set.)
+  backlight.setBrightness(150);
+  // Optional: Set the backlight using RGB values. (Defaults to (10, 128, 128) if not set.)
+  backlight.setRGB(46, 139, 87);
+  // or set the color and brightness at the same time:
+  //backlight.setRGB(46, 139, 87, 150);
   lcd.print("hello, world!");
 }
 
--- examples/SimpleExample/SimpleExample.ino
+++ examples/SimpleExample/SimpleExample.ino
@@ -3,21 +3,16 @@
  * For an RGB LCD with 18 pins, set pin 15 to HIGH.
  * Pin 16 is red, pin 17 is green and pin 18 is blue.
  * On the Arduino board, choose 3 PWM pins (denoted by ~)
- * This sketch will loop through the different colors 
- * and display the color name.
- * This example loops through all of the default colors
- * available in the library.
+ * This sketch will loop through the different colors and display the color name.
+ * This example loops through all of the default colors available in the library.
 */
 
 #include <LiquidCrystal.h>
-
 #include "LCD_BacklightRGB.h"
-
+// Set the pins for the LCD display.
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-// Set the RGB pins for the RGB LCD. 
-// Use PWM pin denoted by ~ symbol.
+// Set the RGB pins for the RGB LCD. Use PWM pins denoted by a ~ symbol.
 const int redPin = 6, greenPin = 9, bluePin = 10;
 LCD_BacklightRGB backlight(redPin, greenPin, bluePin);
 
@@ -26,42 +21,38 @@ void setup() {
   lcd.begin(16, 2);
   // Initiate the RGB pins.
   backlight.begin();
-  // Set the brightness level (0 - 255). (optional)
-  // If not set, the default value is 255
+  // Optional: Set the brightness level (0 - 255). (Defaults to 255 if not set.)
   backlight.setBrightness(150);
-  // Set the backlight using RGB values. (optional)
-  // If not set, the default value is (10, 128, 128)
-  backlight.setRGB(10, 150, 150);
+  // Optional: Set the backlight using RGB values. (Defaults to (10, 128, 128) if not set.)
+  backlight.setRGB(46, 139, 87);
   // or set the color and brightness at the same time:
-  //backlight.setRGB(10, 150, 150, 150);
-  lcd.home();
-  lcd.print("Backlight Color:");
+  //backlight.setRGB(46, 139, 87, 150);
+  lcd.home(); lcd.print("Backlight Color:");
+  lcd.setCursor(0,1); lcd.print("Sea Green1");
+  delay(1000);
 }
 
 void loop() {
-  lcd.setCursor(0,1); 
-  // set the backlight using default colors
+  // Set the backlight using default colors.
   backlight.setRed();
-  lcd.print("Red!     ");
+  // Display the name on the second line of the LCD.
+  lcd.setCursor(0,1); lcd.print("Red!      ");
+  // Change color every second.
   delay(1000);
-  // change color every second
-  lcd.setCursor(0,1); 
+  
   backlight.setYellow();
-  lcd.print("Yellow!  ");
+  lcd.setCursor(0,1); lcd.print("Yellow!   ");
   delay(1000);
 
-  lcd.setCursor(0,1); 
   backlight.setGreen();
-  lcd.print("Green!   ");
+  lcd.setCursor(0,1); lcd.print("Green!    ");
   delay(1000);
 
-  lcd.setCursor(0,1); 
   backlight.setCyan();
-  lcd.print("Cyan!    ");
+  lcd.setCursor(0,1); lcd.print("Cyan!     ");
   delay(1000);
 
-  lcd.setCursor(0,1); 
   backlight.setBlue();
-  lcd.print("Blue!    ");
+  lcd.setCursor(0,1); lcd.print("Blue!     ");
   delay(1000);
 }
