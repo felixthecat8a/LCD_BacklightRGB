146f3f2bcce20f30a0f4a8dada727c987441de6c
Felix <fo59paz@yahoo.com>
2024-11-03 10:48:50 -0600

    added default colors

--- src/Colors.h
+++ src/Colors.h
@@ -6,13 +6,17 @@ struct Colors {
   static const int RED[3];
   static const int ORANGE[3];
   static const int YELLOW[3];
+  static const int LIME[3];
   static const int GREEN[3];
+  static const int TEAL[3];
   static const int CYAN[3];
+  static const int SKY[3];
   static const int BLUE[3];
+  static const int PURPLE[3];
   static const int MAGENTA[3];
+  static const int PINK[3];
   static const int WHITE[3];
   static const int BLACK[3];
-  static const int TEAL[3];
 };
 
 #endif
