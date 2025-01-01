84e296d524260ab53b3fa74a4eb8c9ea5d296199
Felix <fo59paz@yahoo.com>
2025-01-01 10:05:33 -0600

    commented out SimpleExample

--- examples/SimpleExample/SimpleExample.ino
+++ examples/SimpleExample/SimpleExample.ino
@@ -28,66 +28,66 @@
  * 
  */
 
-#include <LiquidCrystal.h>
-#include <LCD_BacklightRGB.h>
-// Set the pins for the LCD display.
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-// Set the RGB pins for the RGB LCD. Use PWM pins denoted by a ~ symbol.
-const int redPin = 6, greenPin = 9, bluePin = 10;
-LCD_BacklightRGB backlight(redPin, greenPin, bluePin);
+// #include <LiquidCrystal.h>
+// #include <LCD_BacklightRGB.h>
+// // Set the pins for the LCD display.
+// const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// // Set the RGB pins for the RGB LCD. Use PWM pins denoted by a ~ symbol.
+// const int redPin = 6, greenPin = 9, bluePin = 10;
+// LCD_BacklightRGB backlight(redPin, greenPin, bluePin);
 
-void setup() {
-  // Initiate the LCD.
-  lcd.begin(16, 2);
-  // Initiate the RGB pins.
-  backlight.begin();
-  // Optional: Set the brightness level (0 - 255).
-  // Defaults to 255 if not set.
-  // backlight.setBrightness(150);
-  // Optional: Set the backlight using RGB values.
-  // Defaults to (10, 135, 143) if not set.
-  // backlight.setRGB(46, 139, 87);
-  // Set the color and brightness:
-  backlight.setRGB(46, 139, 87, 150);
-  lcd.home(); lcd.print("Backlight Color:");
-  lcd.setCursor(0,1); lcd.print("Sea Green!");
-  delay(1000);
-  for (int index = 0; index < 16; index++) {
-    lcd.scrollDisplayLeft(); delay(100);
-  }
-  lcd.clear();
-}
+// void setup() {
+//   // Initiate the LCD.
+//   lcd.begin(16, 2);
+//   // Initiate the RGB pins.
+//   backlight.begin();
+//   // Optional: Set the brightness level (0 - 255).
+//   // Defaults to 255 if not set.
+//   // backlight.setBrightness(150);
+//   // Optional: Set the backlight using RGB values.
+//   // Defaults to (10, 135, 143) if not set.
+//   // backlight.setRGB(46, 139, 87);
+//   // Set the color and brightness:
+//   backlight.setRGB(46, 139, 87, 150);
+//   lcd.home(); lcd.print("Backlight Color:");
+//   lcd.setCursor(0,1); lcd.print("Sea Green!");
+//   delay(1000);
+//   for (int index = 0; index < 16; index++) {
+//     lcd.scrollDisplayLeft(); delay(100);
+//   }
+//   lcd.clear();
+// }
 
-void loop() {
-  // Set the backlight using default colors.
-  backlight.setRed();
-  // Display the name on the second line of the LCD.
-  lcd.setCursor(0,1); lcd.print("Red"); lcd.print("     ");
-  // Change color every second.
-  delay(1000);
+// void loop() {
+//   // Set the backlight using default colors.
+//   backlight.setRed();
+//   // Display the name on the second line of the LCD.
+//   lcd.setCursor(0,1); lcd.print("Red"); lcd.print("     ");
+//   // Change color every second.
+//   delay(1000);
   
-  backlight.setYellow();
-  lcd.setCursor(0,1); lcd.print("Yellow"); lcd.print("     ");
-  delay(1000);
+//   backlight.setYellow();
+//   lcd.setCursor(0,1); lcd.print("Yellow"); lcd.print("     ");
+//   delay(1000);
 
-  backlight.setGreen();
-  lcd.setCursor(0,1); lcd.print("Green"); lcd.print("     ");
-  delay(1000);
+//   backlight.setGreen();
+//   lcd.setCursor(0,1); lcd.print("Green"); lcd.print("     ");
+//   delay(1000);
 
-  backlight.setCyan();
-  lcd.setCursor(0,1); lcd.print("Cyan"); lcd.print("     ");
-  delay(1000);
+//   backlight.setCyan();
+//   lcd.setCursor(0,1); lcd.print("Cyan"); lcd.print("     ");
+//   delay(1000);
 
-  backlight.setBlue();
-  lcd.setCursor(0,1); lcd.print("Blue"); lcd.print("     ");
-  delay(1000);
+//   backlight.setBlue();
+//   lcd.setCursor(0,1); lcd.print("Blue"); lcd.print("     ");
+//   delay(1000);
 
-  backlight.setMagenta();
-  lcd.setCursor(0,1); lcd.print("Magenta"); lcd.print("     ");
-  delay(1000);
+//   backlight.setMagenta();
+//   lcd.setCursor(0,1); lcd.print("Magenta"); lcd.print("     ");
+//   delay(1000);
 
-  backlight.setPink();
-  lcd.setCursor(0,1); lcd.print("Pink"); lcd.print("     ");
-  delay(1000);
-}
+//   backlight.setPink();
+//   lcd.setCursor(0,1); lcd.print("Pink"); lcd.print("     ");
+//   delay(1000);
+// }
